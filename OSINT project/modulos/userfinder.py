135bb4a0399f9aad7be34f89f5871703021ab7ba
gsttps <129559004+gsttps@users.noreply.github.com>
2025-06-08 22:55:28 -0400

    subir archivos.py

---
+++ OSINT project/modulos/userfinder.py
@@ -0,0 +1,52 @@
+import requests
+from colorama import init, Fore #para colorear la salida en consola
+
+init(autoreset=True)
+
+#diccionario rancio de las plataformas donde buscar el user
+PLATAFORMAS = {
+    "GitHub": "https://github.com/{}",
+    "Twitter": "https://twitter.com/{}",
+    "Instagram": "https://www.instagram.com/{}/",
+    "Reddit": "https://www.reddit.com/user/{}",
+    "TikTok": "https://www.tiktok.com/@{}",
+    "Steam": "https://steamcommunity.com/id/{}",
+    "Canva": "https://www.canva.com/{}/",
+    "Xbox": "https://account.xbox.com/en-us/profile?gamertag={}",
+    "PlayStation": "https://my.playstation.com/profile/{}",
+    "Twitch": "https://www.twitch.tv/{}",
+    "Facebook": "https://www.facebook.com/{}",
+    #se pueden agregar mas plataformas, no se me ocurrieron mas xd
+}
+
+def username():
+    while True:
+        user = input(f'{Fore.YELLOW}ingrrese usuario a buscar: {Fore.RESET}')
+        print(f'{Fore.CYAN}\nBuscando usuario: {user}\n{Fore.RESET}')
+        
+        for plataforma, url_patron in PLATAFORMAS.items():
+            try:
+                url = url_patron.format(user)
+                response = requests.get(url, timeout=5)
+                if response.status_code == 200:
+                    print(f'{Fore.GREEN}Usuario encontrado en {plataforma}: {url}{Fore.RESET}')
+                elif response.status_code == 404:
+                    print(f'{Fore.RED}Usuario no encontrado en {plataforma}: {url}{Fore.RESET}')
+                else:
+                    print(f'{Fore.YELLOW}Error al buscar en {plataforma}: {url} (Status code: {response.status_code}){Fore.RESET}')
+                
+                
+            except requests.RequestException as e:
+                print(f"{Fore.MAGENTA}[!] {plataforma}: Error → {e}")
+        
+        
+        continuar = input(f'{Fore.YELLOW}Desea buscar otro usuario? (s/n): {Fore.RESET}')
+        if continuar.lower() != 's':
+            print(f'{Fore.RED}Saliendo del programa...{Fore.RESET}')
+            break
+        
+
+username()
+
+if __name__ == "__main__":
+    username()
